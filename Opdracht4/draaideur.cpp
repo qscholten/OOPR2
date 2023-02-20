2de5c0eeb69dc108b6d76ed5df86b0580614bf2c
qscholten <quig.scholten@outlook.com>
2023-02-20 09:45:51 +0100

    Started Opdracht4

---
+++ Opdracht4/draaideur.cpp
@@ -0,0 +1,40 @@
+#include "draaideur.h"
+#include <QPainter>
+
+DraaiDeur::DraaiDeur(int x, int y, unsigned int lengte, bool liggend): Deur(x,y, lengte), liggend(liggend) {
+}
+
+void DraaiDeur::teken(QPaintDevice *tp) {
+    QPainter p(tp);
+    p.setBrush(Qt::SolidPattern);
+    p.setBrush(Qt::black);
+    QPen pen(Qt::black,2,Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+    p.setPen(pen);
+
+    if(liggend) {
+        if (isDeurOpen()) {
+            if (links) {
+                p.drawLine(this->krijgx(), this->krijgy(), this->krijgx(), this->krijgy()+this->deurLengte());
+            }
+            else {
+                p.drawLine(this->krijgx(), this->krijgy(), this->krijgx(), this->krijgy()-this->deurLengte());
+            }
+        }
+        else {
+            p.drawLine(this->krijgx(), this->krijgy(), this->krijgx()-this->deurLengte(), this->krijgy());
+        }
+    }
+    else {
+        if (isDeurOpen()) {
+            if (links) {
+                p.drawLine(this->krijgx(),this->krijgy(), this->krijgx()-this->deurLengte(), this->krijgy());
+            }
+            else {
+                p.drawLine(this->krijgx(),this->krijgy(), this->krijgx()+this->deurLengte(), this->krijgy());
+            }
+        }
+        else {
+            p.drawLine(this->krijgx(), this->krijgy(), this->krijgx(), this->krijgy()-this->deurLengte());
+        }
+    }
+}
---
+++ Opdracht4/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "deur.h"
+#include <QMainWindow>
+#include <memory>
+
+namespace Ui {
+class MainWindow;
+}
+class Sensor;
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    void paintEvent(QPaintEvent *event);
+      ~MainWindow();
+
+private slots:
+    void on_sensor_act_clicked();
+    void on_schuifdeurKnop_clicked();
+    void on_draaideurKnop1_clicked();
+    void on_draaideurKnop2_clicked();
+
+private:
+    std::shared_ptr<Ui::MainWindow> ui;
+    std::shared_ptr<Sensor> s1;
+    std::array<std::shared_ptr<Deur>,3> deuren = {};
+    std::array<std::shared_ptr<Slot>,5> sloten = {};
+};
+
+#endif // MAINWINDOW_H
